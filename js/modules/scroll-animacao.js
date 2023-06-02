e83b0b1e608966826ceb2db1f4fce187724172ed
Johnny <jpbelem45@outlook.com>
2023-06-02 17:47:40 -0300

    accordion-refatorado

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,6 +1,6 @@
 export default function initAnimacaoScroll() {
   const sections = document.querySelectorAll('[data-anime="scroll"]');
-  const windowMetade = window.innerHeight * 0.75;
+  const windowMetade = window.innerHeight * 0.8;
   function animaScroll() {
     sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
