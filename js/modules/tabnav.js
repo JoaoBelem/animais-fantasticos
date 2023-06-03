1affa33637572dae0c46d32b5f9ff0a7e7aed8c9
Johnny <jpbelem45@outlook.com>
2023-06-03 19:39:45 -0300

    transformando tabNav em classe

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,22 +1,48 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
+// !! Elemento pai de "tabItem" deve ter o atributo "position" do CSS explicito. Para que este plugin funcione da forma devida.
 
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
-    });
-    const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add('ativo', direcao);
+export default class TabNav {
+  constructor(menu, display, initial, activeClassName) {
+    this.tabItem = document.querySelectorAll(menu)
+    this.tabDisplay = document.querySelectorAll(display);
+    this.activeClass = activeClassName;
+    this.initial = initial;
+
+    if (activeClassName === undefined) {
+      this.activeClass = 'ativo';
+    }
+    if (initial === undefined) {
+      this.initial = 0;
+    }
   }
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
-
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
-        activeTab(index);
-      });
+  // Adiciona a classe de ativação
+  activeTab(index) {
+    this.tabDisplay.forEach((section) => {
+      section.classList.remove(this.activeClass);
     });
+    const direcao = this.tabDisplay[index].dataset.anime;
+    this.tabDisplay[index].classList.add(this.activeClass, direcao);
+  }
+
+  // Adiciona classe ativa + muda o menu
+  goto(index) {
+    if (index <= this.tabItem.length - 1 && index >= 0) {
+      this.activeTab(index);
+      this.tabItem[0].parentElement.scroll({
+        top: this.tabItem[index].offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  init(){
+    if (this.tabItem.length && this.tabDisplay.length) {
+      this.tabItem.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+          this.activeTab(index);
+        });
+      });
+      this.goto(this.initial);
+    }
   }
 }
