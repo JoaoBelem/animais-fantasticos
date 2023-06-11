0fb23256208bc28b921adafd77b1c1567a93505a
Johnny <jpbelem45@outlook.com>
2023-06-10 23:10:17 -0300

    refatoração do fetch animais

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,28 +1,30 @@
 import AnimaNumeros from './anima-numeros.js';
 
-export default function initFetchAnimais() {
+export default function FetchAnimais(url, alvo) {
   function createAnimal(animal) {
     const div = document.createElement('div');
     div.classList.add('numero-animal');
     div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
     return div;
   }
-  async function fetchAnimais(url) {
+
+  // Fetch das informações de cada animal e cria o elemento usando createAnimal()
+  async function criarAnimais() {
     try {
       const animaisResponse = await fetch(url);
       const animaisJSON = await animaisResponse.json();
-      const numerosGrid = document.querySelector('.numeros-grid');
+      const numerosGrid = document.querySelector(alvo);
+
       animaisJSON.forEach((animal) => {
         const divAnimal = createAnimal(animal);
         numerosGrid.appendChild(divAnimal);
       });
+      
       const animaNumeros = new AnimaNumeros();
       animaNumeros.init();
     } catch (erro) {
       console.log(erro);
     }
   }
-  fetchAnimais('./animaisapi.json');
-
-
+  criarAnimais();
 }
